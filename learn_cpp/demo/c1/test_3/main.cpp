0a5e7b4b0952447fb27ba3fdc5da4620e945f6d9
git_tianyu <1282920771@qq.com>
2025-02-25 09:27:40 +0800

    修改 - test_3

--- learn_cpp/demo/c1/test_3/main.cpp
+++ learn_cpp/demo/c1/test_3/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream> // 引入输入输出流库
 
+// 定义一个类，负责输出Hello World
+class HelloWorld {
+public:
+    void printMessage() const {
+        std::cout << "Hello, World!" << std::endl; // 输出 "Hello, World!"
+    }
+};
+
 int main() {
-    // 输出 "Hello, World!" 到控制台
-    std::cout << "Hello, World!" << std::endl;
+    HelloWorld hello;  // 创建一个HelloWorld类的对象
+    hello.printMessage(); // 调用printMessage方法输出信息
     return 0; // 返回 0，表示程序成功结束
 }
-
