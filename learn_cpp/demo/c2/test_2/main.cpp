c106add5ef6e5d2914410e404a213bf9c69bd468
tianyu <1282920771@qq.com>
2025-02-26 15:40:13 +0800

    modify for c2

---
+++ learn_cpp/demo/c2/test_2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float f = 3.14f;      // 单精度浮点数
+    double d = 3.14159;   // 双精度浮点数
+
+    cout << "f = " << f << endl;
+    cout << "d = " << d << endl;
+
+    return 0;
+}
---
+++ learn_cpp/demo/c2/test_3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c = 'A';  // 字符类型
+
+    cout << "c = " << c << endl;
+
+    return 0;
+}
+
