53c7f4957ddfebb903aa88f281bb49993a39f967
tianyu <1282920771@qq.com>
2025-02-26 14:53:38 +0800

    add c2

---
+++ learn_cpp/demo/c2/test_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;       // 整数类型
+    short b = 5;      // 短整数类型
+    long c = 100000;  // 长整数类型
+    long long d = 1000000000;  // 更长的整数类型
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
+    return 0;
+}
+
