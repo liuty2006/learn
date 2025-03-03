2a0e56e5c93c3fce121a08922234fff89777da6a
git_tianyu <1282920771@qq.com>
2025-03-03 09:30:23 +0800

    add -c3

---
+++ learn_cpp/demo/c3/c3_1_5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+
+    a += 5;  // a = a + 5
+    cout << "a += 5: " << a << endl;
+
+    a -= 3;  // a = a - 3
+    cout << "a -= 3: " << a << endl;
+
+    a *= 2;  // a = a * 2
+    cout << "a *= 2: " << a << endl;
+
+    return 0;
+}
