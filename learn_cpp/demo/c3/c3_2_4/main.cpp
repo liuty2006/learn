244ced8dc7a86653a5d6b0906af0848979f5578b
git_tianyu <1282920771@qq.com>
2025-03-10 14:09:52 +0800

    修改 - c3

--- learn_cpp/demo/c3/c3_2_4/main.cpp
+++ learn_cpp/demo/c3/c3_2_4/main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 using namespace std;
+#include <bitset>
 
 int main() {
-    int a = 5;  // 二进制：0101
-    int b = 3;  // 二进制：0011
+    int a = 5;  // 二进制：00000101
+    int b = 3;  // 二进制：00000011
 
-    cout << "a & b: " << (a & b) << endl;  // 位与
-    cout << "a | b: " << (a | b) << endl;  // 位或
-    cout << "a ^ b: " << (a ^ b) << endl;  // 位异或
-    cout << "a << 1: " << (a << 1) << endl;  // 左移
-    cout << "a >> 1: " << (a >> 1) << endl;  // 右移
+    // a
+    std::bitset<8> bina(a); // 8 位二进制表示
+    std::cout << "a: " << a << "; binary: " << bina << std::endl;
+
+    // b
+    std::bitset<8> binb(b); // 8 位二进制表示
+    std::cout << "b: " << b << "; binary: " << binb << std::endl;
+
+    // 与
+    int c_and = a & b;
+    std::bitset<8> binc_and(c_and); // 8 位二进制表示
+    std::cout << "c_and: " << c_and << "; binary: " << binc_and << std::endl;
+
+    // 或
+    int c_or = a | b;
+    std::bitset<8> binc_or(c_or); // 8 位二进制表示
+    std::cout << "c_or: " << c_or << "; binary: " << binc_or << std::endl;
+
+    // 异或
+    int c_xor = a ^ b;
+    std::bitset<8> binc_xor(c_xor); // 8 位二进制表示
+    std::cout << "c_xor: " << c_xor << "; binary: " << binc_xor << std::endl;
+
+    // 左移
+    int c_left = a << 1;
+    std::bitset<8> binc_left(c_left); // 8 位二进制表示
+    std::cout << "c_left: " << c_left << "; binary: " << binc_left << std::endl;
+
+    // 右移
+    int c_right = a >> 1;
+    std::bitset<8> binc_right(c_right); // 8 位二进制表示
+    std::cout << "c_right: " << c_right << "; binary: " << binc_right << std::endl;
 
     return 0;
 }
--- learn_cpp/demo/c3/c3_4_3/main.cpp
+++ learn_cpp/demo/c3/c3_4_3/main.cpp
@@ -8,6 +8,12 @@ int main() {
     double pi = 3.14159265358979;
 
     // ================== 设置浮点数精度 ==================
+
+    // setprecision(n) 直接作用于所有有效数字（不固定小数点）
+    cout << "Pi with 2 decimal places: " << setprecision(2) << pi << endl;
+    cout << "Pi with 4 decimal places: " << setprecision(4) << pi << endl;
+
+    // fixed 让 setprecision(n) 作用于小数点后的位数。
     cout << "Pi with 2 decimal places: " << fixed << setprecision(2) << pi << endl;
     cout << "Pi with 4 decimal places: " << fixed << setprecision(4) << pi << endl;
 
@@ -18,7 +24,7 @@ int main() {
     cout << hex << "十六进制: " << num << endl;
     cout << oct << "八进制: " << num << endl;
 
-    cout << showbase;
+    cout << showbase;       // showbase 让 hex 输出 0x 前缀，oct 输出 0 前缀
     cout << "showbase 十六进制: " << hex << num << endl;
     cout << "showbase 八进制: " << oct << num << endl;
 
